4533e4b365e082f55541e4a8d1c95e07c574d3de
Ran <ransogorb@outlook.com>
2026-03-10 13:14:07 +0100

    Fetching de la API hecho para Estudiante
    He hecho el fetching de datos del Doctype Estudiante y he mapeado los datos en el perfil. Ahora los datos se pueden modificar.

--- App_Alumno/hooks/useStudent.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getStudentProfile } from '../services/studentService';
-
-export const useStudent = () => {
-  return useQuery({
-    queryKey: ['student'],
-    queryFn: getStudentProfile,
-  });
-};
