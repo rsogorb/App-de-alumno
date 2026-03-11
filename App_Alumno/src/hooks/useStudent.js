a0267ef5fc725f965fb11efaff13739ae939b199
Ran <ransogorb@outlook.com>
2026-03-11 15:06:05 +0100

    Cambiado el index para que salude con nombre y ajustes en la imagen.

--- App_Alumno/src/hooks/useStudent.js
+++ App_Alumno/src/hooks/useStudent.js
@@ -5,11 +5,8 @@ export const useStudent = (dniAlumno) => {
   return useQuery({
     queryKey: ['student', dniAlumno],
     queryFn: () => getStudentProfile(dniAlumno),
-    // Esto es clave: si dniAlumno es null o undefined, no intenta hacer la petición
     enabled: !!dniAlumno, 
-    // Opcional: mantiene los datos anteriores mientras carga los nuevos
     keepPreviousData: true, 
-    // Opcional: evita que re-intente 3 veces si falla (útil en desarrollo)
     retry: false,
   });
 };
